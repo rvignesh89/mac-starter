978730f54a0c43d6cbf666c3fbb0766f900ec18d
Vignesh Rajasekaran <vignesh@rvignesh.io>
2020-08-25 00:01:43 +0800

    Fix install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-mkdir -p ~/.mac-starter
+mkdir -p ~/.mac-starter && cd ~/.mac-starter
 
-curl -L -O https://github.com/rvignesh89/mac-starter/releases/download/0.0.1/mac-starter.zip -o ~/.mac-starter/mac-starter.zip
+curl -L -O https://github.com/rvignesh89/mac-starter/releases/download/0.0.1/mac-starter.zip 
 
-unzip ~/.mac-starter/mac-starter.zip -d ~/.mac-starter
+unzip ~/.mac-starter/mac-starter.zip 
 
 sudo ~/.mac-starter/bootstrap.sh
