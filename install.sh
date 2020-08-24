ad244c9d999b96e0e563dd06c91e7f44dc3f8ca0
Vignesh Rajasekaran <vignesh@rvignesh.io>
2020-08-24 23:27:39 +0800

    Add install script which is runnable using curl

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+
+mkdir -p ~/.mac-starter
+
+curl https://github.com/rvignesh89/mac-starter/releases/download/0.0.1/mac-starter.zip -o ~/.mac-starter/mac-starter.zip
+
+unzip ~/.mac-starter/mac-starter.zip -d ~/.mac-starter
+
+sudo ~/.mac-starter/mac-starter/bootstrap.sh
